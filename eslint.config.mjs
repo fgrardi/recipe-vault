b29d34a73b75d564d1143d7b840f19238962b053
fiengerardi <fien.gerardi@littlemissrobot.com>
2025-03-07 08:39:22 +0100

    feat(wip): eslint optimizations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import prettierConfig from "eslint-config-prettier";
+import prettier from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import prettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,7 +17,11 @@ const eslintConfig = [
 	...compat.extends("next/core-web-vitals", "next/typescript"),
 	prettierConfig,
 	{
-		plugins: { prettier },
+		plugins: {
+			prettier,
+			"unused-imports": unusedImports,
+			"simple-import-sort": simpleImportSort,
+		},
 		rules: {
 			"prettier/prettier": ["warn", { useTabs: true, tabWidth: 4 }],
 			indent: ["error", "tab"],
@@ -27,6 +33,24 @@ const eslintConfig = [
 				{ blankLine: "always", prev: "*", next: "block-like" },
 				{ blankLine: "always", prev: "*", next: "export" },
 			],
+			// remove unused imports
+			"no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+			"unused-imports/no-unused-imports": "error",
+			"unused-imports/no-unused-vars": [
+				"warn",
+				{
+					vars: "all",
+					varsIgnorePattern: "^_",
+					args: "after-used",
+					argsIgnorePattern: "^_",
+				},
+			],
+			// sorting imports
+			"simple-import-sort/imports": "error",
+			"simple-import-sort/exports": "error",
+			"import/first": "error",
+			"import/newline-after-import": "error",
+			"import/no-duplicates": "error",
 		},
 	},
 ];
